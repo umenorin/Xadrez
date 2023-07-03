3d68ac6652f9bd5160d16787bc242dd9b2cccd61
Umenorin <umenorin@gmail.com>
2023-07-03 20:51:02 -0300

    criacao da classe posicao

--- xadrez_console/Program.cs
+++ xadrez_console/Program.cs
@@ -1,7 +1,11 @@
-﻿namespace xadrez_console {
+﻿using Tabuleiro;
+
+namespace xadrez_console {
     class Program {
         static void Main(string[] args) {
+            posicao p = new posicao(3, 4);
 
+            Console.WriteLine(p.ToString());
         }
     }
 }
---
+++ xadrez_console/Tabuleiro/posicao.cs
@@ -0,0 +1,16 @@
+﻿
+namespace Tabuleiro {
+    internal class posicao {
+        public int Linha { get; set; }
+        public int Coluna { get; set; }
+
+        public posicao(int linha, int colina) { 
+            Linha = linha;
+            Coluna = colina;
+        }
+
+        public override string ToString() {
+            return $"{Linha} , {Coluna}";
+        }
+    }
+}
