990a6fbdc534327f9da742e2f091aa114fb94e0a
Umenorin <umenorin@gmail.com>
2023-07-04 18:37:10 -0300

    tratando erros  de posicionamento das pecas

---
+++ xadrez_console/Tabuleiro/Exception/TabuleiroExeption.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tabuleiro.Exception {
+    internal class tabuleiroException : ApplicationException {
+
+        public tabuleiroException(string message) : base(message) {
+        }
+    }
--- xadrez_console/Tabuleiro/Tabuleiro.cs
+++ xadrez_console/Tabuleiro/Tabuleiro.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Tabuleiro.Exception;
 
 namespace tabuleiro {
     internal class Tabuleiro {
@@ -16,9 +17,30 @@ namespace tabuleiro {
             return Pecas[linha, coluna];
         }
 
+        public  Peca Peca(Posicao pos) {
+            return Pecas[pos.Linha, pos.Coluna];
+        }
+
+        public bool existePeca(Posicao pos) {
+            if (existePeca(pos))
+                throw new tabuleiroException("ja existe uma peca nessa posicao");
+            validarPosicao(pos);
+            return Peca(pos) != null;
+        }
+
         public void colocarPeca(Peca p, Posicao pos) {
             Pecas[pos.Linha,pos.Coluna] = p;
             p.Posicao = pos;
+        }
+
+        public bool posicaoValida(Posicao pos) {
+            if(pos.Linha < 0|| pos.Linha>=Linha|| pos.Coluna<0 || pos.Coluna>=Coluna) 
+                return false;
+            return true;
+        }
+        public void validarPosicao(Posicao pos) {
+            if (!posicaoValida(pos))
+                throw new tabuleiroException("Posicao invalida");
         }
     }
 }
